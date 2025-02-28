8cd0f65e3b0b18e42d936753b0231cd99c29ecd5
gaborgr <gaborgr@gmail.com>
2025-02-28 11:09:34 -0300

    Lanzamiento de este paquete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="miQuintoRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Gabriel Guerra",                         # Tu nombre
+    author_email="gaborgr@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/gaborgr/miQuintoRepo",     # URL del proyecto
+)
